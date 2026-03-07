9df5c71b9ee736baa68a394a1e25d37548853413
Sofia Alvarado <123610145+Sofia17266@users.noreply.github.com>
2026-03-06 19:52:39 -0600

    Fix structure: move projects back inside NewsHub folder

--- NewsHub.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NewsHub.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
